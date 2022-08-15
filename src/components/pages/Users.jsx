21cd38b22e1d58f05991dcb83e30825876645e4d
akinus <42932934+akimo10@users.noreply.github.com>
2022-08-15 09:56:56 +0900

    contextを利用したグローバルなStateを実装

--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -17,16 +17,13 @@ const users = [...Array(10).keys()].map((val) => {
 });
 
 export const Users = () => {
-  const { state } = useLocation();
-  console.log(state);
-  const isAdmin = state ? state.isAdmin : false;
   return (
     <SContainer>
       <h2>ユーザー一覧</h2>
       <SearchInput />
       <SUserArea>
         {users.map((user) => (
-          <UserCard key={user} user={user} isAdmin={isAdmin} />
+          <UserCard key={user} user={user} />
         ))}
       </SUserArea>
     </SContainer>
